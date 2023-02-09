b5a1d90f8a6de6238981ae6f8aee2c68ace9cdf7
AntonAnatoliyovich <anton.shadura@gmail.com>
2023-02-09 14:18:47 +0200

    init

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import debounce from 'lodash.debounce';
+import fetchCountries from './js/fetchCountries';
+import renderCountries from './js/renderCountries';
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+const input = document.querySelector("#search-box");
+
+input.addEventListener('input', debounce(searchCountries, DEBOUNCE_DELAY));
+
+function searchCountries(event) {
+    let countryName = event.target.value.trim();
+    fetchCountries(countryName)
+    .then(countriesObjects => {
+        renderCountries(countriesObjects);
+    })
+    .catch(error => {
+        Notify.failure("Oops, there is no country with that name");
+    })
+}
---
+++ src/js/renderCountries.js
@@ -0,0 +1,23 @@
+export default function renderCountries(countriesObjects) {
+    let countryLength = countriesObjects.length;
+    let countries = [];
+
+    const countriesList = document.querySelector(".country-list");
+
+    if(countryLength > 10) {
+        console.log("Too many matches found. Please enter a more specific name.");
+    } else if(countryLength >= 2 && countryLength <= 10) {
+        countriesObjects.map(country => {
+            countries += `<li class="country">
+            <img src="${country.flags.svg}" width=20/>
+            <span>${country.name}</span>
+            </li>`
+            countriesList.innerHTML = countries;
+        })
+    } else {
+        let country = countriesObjects[0];
+        console.log(country);
+    }
+}
+
+
