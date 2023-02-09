11d7e5fecadfaf1b883d4a87577652bde24af7f9
AntonAnatoliyovich <anton.shadura@gmail.com>
2023-02-09 19:21:11 +0200

    init

--- src/js/renderCountries.js
+++ src/js/renderCountries.js
@@ -1,24 +1,37 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 export default function renderCountries(countriesObjects) {
     let countryLength = countriesObjects.length;
     let countries = [];
 
     const countriesList = document.querySelector(".country-list");
-     // const countryInfo
+    const countryInfo = document.querySelector(".country-info");
 
-    if(countryLength > 10) {
-        console.log("Too many matches found. Please enter a more specific name.");
-    } else if(countryLength >= 2 && countryLength <= 10) {
-        countriesObjects.map(country => {
-            countries += `<li class="country">
+    if(countryLength < 2) {
+        let country = countriesObjects[0];
+        // console.log(country);
+        countryInfo.innerHTML = `
+        <div>
             <img src="${country.flags.svg}" width=25/>
             <span>${country.name}</span>
-            </li>`
-            countriesList.innerHTML = countries;
+        </div>
+        <div class="countryCard">
+            <span>Capital: ${country.capital}</span>
+            <span>Population: ${country.population}</span>
+            <span>Languages: ${country.languages[0].name}</span>
+        </div>
+        `
+    } else if(countryLength >= 2 && countryLength <= 10) {
+        countriesObjects.map(country => {
+        countries += `
+        <li class="country">
+            <img src="${country.flags.svg}" width=25/>
+            <span>${country.name}</span>
+        </li>
+        `
+        countriesList.innerHTML = countries;
         })
-    } else {
-        let country = countriesObjects[0];
-        // countryInfo.innerHTML = ``
+        } else {
+        Notify.info("Too many matches found. Please enter a more specific name.");
     }
 }
-
-
